112527b40e9d98072f6be0eeea827b9fd279082a
sahadporn charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2020-09-24 02:05:28 +0700

    add more unittest

--- mysite/polls/test.py
+++ mysite/polls/test.py
@@ -135,6 +135,17 @@ from django.urls import reverse
 #                      pub_date=self.past, end_date=self.past + datetime.timedelta(days=2))
 #         self.assertFalse(q.can_vote())
 
+
+def create_question(question_text, days):
+    """
+        Create a question with the given 'question_text' and published
+        the given number of 'days' offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time, end_date=time + datetime.timedelta(days=334))
+
+
 class QuestionIndexViewTest(TestCase):
 
     def setUp(self):
@@ -152,30 +163,98 @@ class QuestionIndexViewTest(TestCase):
             self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_past_question(self):
-            """
-            Test is there is an already published question must be display on index page.
-            """
-            response = self.client.get(reverse('polls:index'))
-            q = Question.objects.create(question_text="Hello?",
-                         pub_date=self.now, end_date=self.future)
-            self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Hello?>'])
+        """
+        Test is there is an already published question must be display on index page.
+        """
+        q2 = Question.objects.create(question_text="Hello?",
+                                     pub_date=self.past, end_date=(self.future + datetime.timedelta(days=2)))
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [
+                                 '<Question: Hello?>'])
 
     def test_future_question(self):
             """
             Test future question must not be display.
             """         
-            response = self.client.get(reverse('polls:index'))
             q = Question.objects.create(question_text="Hello?",
                              pub_date=self.future, end_date=self.future + datetime.timedelta(days=2))
+            response = self.client.get(reverse('polls:index'))
             self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_future_and_past_question(self):
         """
         Test index with both future and past question. Only past question will be display.
         """
-        response = self.client.get(reverse('polls:index'))
         q = Question.objects.create(question_text="Hi?",
                      pub_date=self.future, end_date=self.future + datetime.timedelta(days=2))
         q2 = Question.objects.create(question_text="Hello?",
                                          pub_date=self.past, end_date=self.future + datetime.timedelta(days=2))
+        response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Hello?>'])
+
+    def test_two_published_poll(self):
+        q = Question.objects.create(question_text="Hi?",
+                                    pub_date=self.past, end_date=self.future + datetime.timedelta(days=2))
+        q2 = Question.objects.create(question_text="Hello?",
+                                     pub_date=self.past, end_date=self.future + datetime.timedelta(days=2))
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [
+                                 '<Question: Hi?>', '<Question: Hello?>'])
+
+    def test_redirect_root_url(self):
+        """
+        root url should redirect to polls index.
+        """                         
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        pollURL = reverse('polls:index')
+        self.assertRedirects(response, pollURL)
+
+    def test_poll_index(self):
+        q = Question.objects.create(question_text="Hi?",
+                                    pub_date=self.past, end_date=self.future + datetime.timedelta(days=2))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'polls/index.html')
+        self.assertIn(str.encode(q.question_text), response.content)
+
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """
+        Detail view with future question should return 404 not found.
+        """
+        fu = Question.objects.create(question_text="Hi?", pub_date=timezone.now() + datetime.timedelta(days=1), end_date=timezone.now() + datetime.timedelta(days=2))
+        url = reverse('polls:detail', args=(fu.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_past_question(self):
+        q = Question.objects.create(question_text="Hi?", pub_date=timezone.now(
+        ) - datetime.timedelta(days=1), end_date=timezone.now() + datetime.timedelta(days=2))
+        url = reverse('polls:detail', args=(q.id,))
+        response = self.client.get(url)
+        self.assertContains(response, q.question_text)
+    
+    def test_ended_question(self):
+        q = Question.objects.create(question_text="Hi?", pub_date=timezone.now(
+        ) - datetime.timedelta(days=2), end_date=timezone.now() - datetime.timedelta(days=1))
+        url = reverse('polls:detail', args=(q.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('polls:index'))
+        self.assertIn(response.content, str.encode("ended"))
+
+    def test_detail_with_wrong_question_id(self):
+        response = self.client.get(reverse('polls:detail', args=("1",)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_in_detail(self):
+        q = Question.objects.create(question_text="Hi?", pub_date=timezone.now(
+        ) - datetime.timedelta(days=1), end_date=timezone.now() + datetime.timedelta(days=2))
+        c = Choice.objects.create(choice_text="Nope", question=q)
+        c1 = Choice.objects.create(choice_text="Sure", question=q)
+        a = self.client.get('polls:results', args=(q.id,))
+        self.assertEqual(a.status_code, 404)
+        response = self.client.post('/polls/{q.id}/', {'choice': '2'})
+        self.assertEqual(response.status_code, 404)
+        # self.assertRedirects(a, response)
